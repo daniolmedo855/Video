27192de49189e1c6a59e8660ae6f128112792194
daniolmedo855 <daniolmedo855@gmail.com>
2024-05-13 13:45:52 +0200

    mi primer commit

---
+++ src/randomworldgenerator/RandomWorldGenerator.java
@@ -0,0 +1,10 @@
+package randomworldgenerator;
+
+public class RandomWorldGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Git");
+    }
+    
+}
