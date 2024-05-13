01c51028c64fcf628fc0a5c56edc2f765a1b1643
alumnosi <daniolmedo855@gmail.com>
2024-05-13 13:59:52 +0200

    mi primer commit desde linux

--- src/randomworldgenerator/RandomWorldGenerator.java
+++ src/randomworldgenerator/RandomWorldGenerator.java
@@ -5,6 +5,7 @@ public class RandomWorldGenerator {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Adios mundo");
+        System.out.println("Estoy trabajando desde Linux");
     }
     
 }
